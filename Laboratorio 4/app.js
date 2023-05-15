11cd3141a7365d0430e17aa18930e6f617468270
Jean Guevara <jean.guevara@consentussg.com>
2023-05-15 09:03:46 -0500

    Se realizan cambios en el CSS, Se agrega recorrido del foreach y fuction, se remove remove.js que no se utilizo

--- Laboratorio 4/app.js
+++ Laboratorio 4/app.js
@@ -1,73 +1,73 @@
 (() => {
   const App = {
   htmlElements: {
-  operationsForm: document.getElementById('operations'),
+    operationsForm: document.getElementById('operations'),
   },
   init: () => {
-  console.log('Inicio de la App ...');
-  App.methods.mostrarSecuenciaDeFibonacci({num})
-  App.htmlElements.operationsForm.addEventListener(
-  'submit',
-  App.handlers.operationsForm
+    console.log('Inicio de la App ...');
+    App.methods.mostrarSecuenciaDeFibonacci({num})
+    App.htmlElements.operationsForm.addEventListener(
+    'submit',
+    App.handlers.operationsForm
     );
   },
   methods: {
-  mostrarSecuenciaDeFibonacci({num}) {
-  let fibonacci = [];
-  let x = 0;
-  for (x; x<num; x++){
-  if(x==0){
-  fibonacci.push(0);
-  }
-  else if(x==1){
-  fibonacci.push(1);
-  }
-  else{
-  fibonacci.push(fibonacci[x-1]+fibonacci[x-2]);
+    mostrarSecuenciaDeFibonacci({num}) {
+    let fibonacci = [];
+    let x = 0;
+    for (x; x<num; x++){
+    if(x==0){
+    fibonacci.push(0);
     }
-  }
+    else if(x==1){
+    fibonacci.push(1);
+    }
+    else{
+    fibonacci.push(fibonacci[x-1]+fibonacci[x-2]);
+      }
+    }
   return fibonacci
   }, 
   printSequenceOnCards(sequence){
-  const cardsContainer = document.getElementById("respond");
-  cardsContainer.innerHTML = "";
-  sequence.forEach((numero) => {
-  const card = document.createElement("div");
-  card.classList.add("card-respond");
-  card.id=("card"+numero)
-  card.innerHTML = numero;
-  cardsContainer.appendChild(card);
-    });
+    const cardsContainer = document.getElementById("respond");
+    cardsContainer.innerHTML = "";
+    sequence.forEach(Myfunction);
+    function Myfunction(item,index) {
+    const card = document.createElement("div");
+    card.classList.add("card-respond");
+    card.id=("card"+index);
+    card.innerHTML = item;
+    cardsContainer.appendChild(card);
+    }
   },
   CrearLabel(){
-
-  const myNodelist = document.getElementsByClassName("card-respond");
-  for (let i = 0; i < myNodelist.length; i++) {
-    const lb = document.createElement("label");
-    lb.classList.add("lb");
-    lb.id=("lb"+i);
-    lb.innerHTML = "X";
-    myNodelist[i].appendChild(lb);
+    const myNodelist = document.getElementsByClassName("card-respond");
+    for (let i = 0; i < myNodelist.length; i++) {
+      const lb = document.createElement("label");
+      lb.classList.add("lb");
+      lb.id=("lb"+i);
+      lb.innerHTML = "X";
+      myNodelist[i].appendChild(lb);
     }     
   },
   DeleteCardElementOnClick(){
-  const cardsContainer = document.getElementById("respond");
-  cardsContainer.onmouseenter = function() {
-  const myNodelist = document.getElementsByClassName("card-respond");
-  const myNodelistlb = document.getElementsByClassName("lb");
-  console.log(myNodelistlb)
-  for (let i = 0; i < myNodelist.length; i++) {
-  const lb = document.getElementById(myNodelist[i].id);
-  const lb2 = document.getElementById(myNodelistlb[i].id);
-  lb2.onmouseenter = function() {
-  lb2.style.color = "red";
-  }
-  lb2.onmouseleave = function() {
-  lb2.style.color = "white";
-  }
-  lb.onclick = function() {
-  alert("Esta apunto de Eliminar el Elemento Card")
-  lb.parentNode.removeChild(lb);
+    const cardsContainer = document.getElementById("respond");
+    cardsContainer.onmouseenter = function() {
+    const myNodelist = document.getElementsByClassName("card-respond");
+    const myNodelistlb = document.getElementsByClassName("lb");
+    console.log(myNodelistlb)
+    for (let i = 0; i < myNodelist.length; i++) {
+    const lb = document.getElementById(myNodelist[i].id);
+    const lb2 = document.getElementById(myNodelistlb[i].id);
+    lb2.onmouseenter = function() {
+    lb2.style.color = "red";
+    }
+    lb2.onmouseleave = function() {
+    lb2.style.color = "white";
+    }
+    lb.onclick = function() {
+    alert("Esta apunto de Eliminar el Elemento Card")
+    lb.parentNode.removeChild(lb);
       }
     }
   }
