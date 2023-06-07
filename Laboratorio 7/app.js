d7ee55c8ec044b648e968e65189be2db9cacccef
Jean Guevara <jean.guevara@consentussg.com>
2023-06-07 16:45:32 -0500

    Laboratorio 7 reduccion de lineas de codigo

--- Laboratorio 7/app.js
+++ Laboratorio 7/app.js
@@ -1,30 +1,26 @@
 const {get, post } = require("./utils.js")
     const App = {
         async init(pokeName){
-        const resp = await get(`https://pokeapi.co/api/v2/pokemon/${pokeName}`);
-        const resp2 = await get(resp.species.url);
-        const resp3 = await get(resp2.evolution_chain.url);
+        const json = await get(`https://pokeapi.co/api/v2/pokemon/${pokeName}`);
+        const json2 = await get(json.species.url);
+        const json3 = await get(json2.evolution_chain.url);
         const abilities = [];
         const Evolution = [];
-        resp.abilities.forEach(element => {
+        json.abilities.forEach(element => {
             abilities.push(element.ability.name);
         });
-        Evolution.push(resp3.chain.species.name);
-        resp3.chain.evolves_to.forEach(element => {
+        Evolution.push(json3.chain.species.name);
+        json3.chain.evolves_to.forEach(element => {
             Evolution.push(element.species.name);
         });
-        resp3.chain.evolves_to.forEach(element => {
+        json3.chain.evolves_to.forEach(element => {
             element.evolves_to.forEach(element => {
                 Evolution.push(element.species.name);
             });
         });
         let pokemon = {
-            name: resp.name,
-            id: resp.id,
-            weight: resp.weight,
-            height: resp.height,
-            abilities: abilities,
-            evolves_to: Evolution,
+            name: json.name, id: json.id, weight: json.weight, height: json.height,
+            abilities: abilities, evolves_to: Evolution,
         }
         console.log({pokemon});
         },
