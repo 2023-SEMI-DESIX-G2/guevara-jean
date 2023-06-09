e4b277b8c784d232fe060234aade8121d639fb70
Jean Guevara <jean.guevara@consentussg.com>
2023-06-09 15:45:45 -0500

    Laboratorio 8 Update

--- Laboratorio 8/app.js
+++ Laboratorio 8/app.js
@@ -4,7 +4,7 @@ const app = express()
 const PORT = 3000
 
 app.get('/', function (req, res) {
-  res.send('<h2>HELLO JEAN PAUL GUEVARA</h2>')
+  res.send('<h2 style="display: flex;justify-content: center;margin-top: 75px;font-size: 48;color: firebrick;font-family: monospace;">HELLO JEAN PAUL GUEVARA</h2><br><a style="display: flex;justify-content: center;font-size: 28;color: firebrick; font-family: monospace;" href="http://localhost:3000/pokemon/list/eevee">!Click Here!</a>')
 })
 app.get('/pokemon/json/:name', async function (req, res) {
     const pokeName = req.params.name;
