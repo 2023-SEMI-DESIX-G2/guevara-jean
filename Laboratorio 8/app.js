b9f98e9d1316bb2cdc83cd79613b01bb98f0e831
Jean Guevara <jean.guevara@consentussg.com>
2023-06-09 14:52:17 -0500

    Laboratorio 8 Vista ul lista y Json

--- Laboratorio 8/app.js
+++ Laboratorio 8/app.js
@@ -6,15 +6,6 @@ const PORT = 3000
 app.get('/', function (req, res) {
   res.send('<h2>HELLO JEAN PAUL GUEVARA</h2>')
 })
-
-app.get('/saludo', function (req, res) {
-    res.json({mensage: "Hola Jean Paul"})
-})
-
-app.get('/pokemon', function (req, res) {
-    res.json({mensage: "Hola Jean Paul"})
-})
-
 app.get('/pokemon/json/:name', async function (req, res) {
     const pokeName = req.params.name;
     res.json(await getPoke(pokeName))
@@ -23,7 +14,7 @@ app.get('/pokemon/list/:name', async function (req, res) {
     const pokeName = req.params.name;
     const obj =  await getPoke(pokeName)
     console.log(obj.name)
-    res.send(`<h2 style="
+    res.send(`<body><h2 style="
     font-size: 36px;
     font-family: monospace;
     display: flex;
@@ -38,6 +29,6 @@ app.get('/pokemon/list/:name', async function (req, res) {
     <li>Height---->${obj.height}</li>
     <li>AilitiesList---->${obj.abilities}</li>
     <li>EvolestoList---->${obj.evolves_to}</li>
-    </ul>`)
+    </ul></body>`)
 })
 app.listen(PORT, () => {console.log(`Servidor Iniciando en el Puerto# ${PORT}`)})
