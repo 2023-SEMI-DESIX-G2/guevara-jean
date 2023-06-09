cdcf4771b2a23096b6147188cccef7f9a1e01c97
Jean Guevara <jean.guevara@consentussg.com>
2023-06-09 09:50:09 -0500

    Laboratorio 8 json y ulLista vista

--- Laboratorio 8/app.js
+++ Laboratorio 8/app.js
@@ -1,7 +1,6 @@
 const {getPoke} = require("./pokemon.js")
 const express = require('express')
 const app = express()
-
 const PORT = 3000
 
 app.get('/', function (req, res) {
@@ -16,9 +15,29 @@ app.get('/pokemon', function (req, res) {
     res.json({mensage: "Hola Jean Paul"})
 })
 
-app.get('/pokemon/:name', async function (req, res) {
+app.get('/pokemon/json/:name', async function (req, res) {
     const pokeName = req.params.name;
     res.json(await getPoke(pokeName))
 })
-
+app.get('/pokemon/list/:name', async function (req, res) {
+    const pokeName = req.params.name;
+    const obj =  await getPoke(pokeName)
+    console.log(obj.name)
+    res.send(`<h2 style="
+    font-size: 36px;
+    font-family: monospace;
+    display: flex;
+    align-items: center;
+    justify-content: center;">/Lista Pokemon/</h2><ul style="
+    font-size: 26;
+    font-family: monospace;
+    color: darkslateblue;">
+    <li>ID----> ${obj.id}</li>
+    <li>NamePokemon---->${obj.name}</li>
+    <li>Weight---->${obj.weight}</li>
+    <li>Height---->${obj.height}</li>
+    <li>AilitiesList---->${obj.abilities}</li>
+    <li>EvolestoList---->${obj.evolves_to}</li>
+    </ul>`)
+})
 app.listen(PORT, () => {console.log(`Servidor Iniciando en el Puerto# ${PORT}`)})
