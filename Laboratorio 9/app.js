96f55166d4b6939d8e1512886a66954316e18389
Jean Guevara <jean.guevara@consentussg.com>
2023-06-20 10:41:24 -0500

    Laboratorio 9 Update

--- Laboratorio 9/app.js
+++ Laboratorio 9/app.js
@@ -1,4 +1,4 @@
-const {getPoke} = require("./pokemon.js")
+const {pokemon} = require("./pokemon.js")
 const express = require('express')
 const app = express()
 const PORT = 3000
@@ -6,13 +6,38 @@ const PORT = 3000
 app.get('/', function (req, res) {
   res.send('<h2 style="display: flex;justify-content: center;margin-top: 75px;font-size: 48;color: firebrick;font-family: monospace;">HELLO JEAN PAUL GUEVARA</h2><br><a style="display: flex;justify-content: center;font-size: 28;color: firebrick; font-family: monospace;" href="http://localhost:3000/pokemon/list/eevee">!Click Here!</a>')
 })
+app.get("/cache", (req, res) => {
+  res.json(pokemon.getCache());
+});
+
+app.get("/:id", async (req, res) => {
+  const obj = await pokemon.get(req.params.id);
+  console.log(obj)
+  // res.json(response);
+  res.send(`<body><h2 style="
+    font-size: 36px;
+    font-family: monospace;
+    display: flex;
+    align-items: center;
+    justify-content: center;">/Lista Pokemon/</h2><ul style="
+    font-size: 26;
+    font-family: monospace;
+    color: darkslateblue;">
+    <li>ID----> ${obj.pokemon.id}</li>
+    <li>NamePokemon---->${obj.pokemon.name}</li>
+    <li>Weight---->${obj.pokemon.weight}</li>
+    <li>Height---->${obj.pokemon.height}</li>
+    <li>AilitiesList---->${obj.pokemon.abilities}</li>
+    <li>EvolestoList---->${obj.pokemon.evolves_to}</li>
+    </ul></body>`)
+});
 app.get('/pokemon/json/:name', async function (req, res) {
-    const pokeName = req.params.name;
-    res.json(await getPoke(pokeName))
+    const pokemonId = req.params.name;
+    res.json(await getPoke(pokemonId))
 })
 app.get('/pokemon/list/:name', async function (req, res) {
-    const pokeName = req.params.name;
-    const obj =  await getPoke(pokeName)
+    const pokemonId = req.params.name;
+    const obj =  await getPoke(pokemonId)
     console.log(obj.name)
     res.send(`<body><h2 style="
     font-size: 36px;
